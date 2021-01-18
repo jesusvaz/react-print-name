b958318bd99d5065c5efa4a1f3c61114668e2ace
Jesus Vazquez <jesusvazquez@jesuss-mac-mini.lan>
2021-01-18 12:09:04 -0700

    Add name

--- src/App.js
+++ src/App.js
@@ -1,23 +1,10 @@
 import logo from './logo.svg';
 import './App.css';
-
+//git remote add origin https://github.com/jesusvaz/react-print-name.git
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>Jesus Vazquez sauceda</h2>
     </div>
   );
 }
